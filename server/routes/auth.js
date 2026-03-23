a72cfcee8312f65226953b8d0c03af13620c320d
pymom2026 <servicenowni@gmail.com>
2026-03-23 16:45:36 -0700

    Update Google OAuth callback URL to use environment variable

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,7 +8,7 @@ const router = express.Router();
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback',
+  callbackURL: process.env.CALLBACK_URL,
   scope: [
     'profile',
     'email',
